07397c9c3066b4cfedf0c2aa3b2deafe8e9d460a
stephanH <rabadjiev@gmail.com>
2018-10-20 14:30:33 +0300

    add

---
+++ MySqlCommon.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Reflection;
+using MySql.Data.MySqlClient;
+
+namespace RootNamespace.Persistence
+{
+    internal class MySqlCommon
+    {
+        //private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly Logger Log = new Logger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static MySqlConnection Connection { get; set; }
+
+        public static MySqlCommand GetCommand(string connectionString, string sql, IReadOnlyCollection<MySqlParameter> parameters)
+        {
+            Connect(connectionString);
+
+            var command = new MySqlCommand
+            {
+                Connection = Connection,
+                CommandText = sql
+            };
+            command.Prepare();
+            if (parameters != null)
+            {
+                foreach (var mySqlParameter in parameters)
+                {
+                    command.Parameters.Add(mySqlParameter);
+                }
+            }
+
+            return command;
+        }
+
+        private static void Connect(string connectionString)
+        {
+            if (Connection == null || Connection.State != ConnectionState.Open)
+            {
+                Log.Info($"mysql connection started");
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                Connection = new MySqlConnection
+                {
+                    ConnectionString = ConfigurationManager.ConnectionStrings[connectionString].ConnectionString
+                };
+                Connection.Open();
+                stopwatch.Stop();
+                Log.Info($"mysql connected for {stopwatch.ElapsedMilliseconds} milliseconds ");
+            }
+        }
+
+        //public static bool Ping(string connectionString)
+        //{
+        //    Connect(connectionString);
+        //    return Connection.Ping();
+        //}
+    }
+}
---
+++ ProcessCommon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace RootNamespace.Persistence
+{
+    public class ProcessCommon
+    {
+        //private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly Logger Log = new Logger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void KillProcessesByName(string processName)
+        {
+            foreach (var process in Process.GetProcessesByName(processName))
+            {
+                process.Kill();
+            }
+
+        }
+
+        public static void KillOldInstances()
+        {
+            var pname = Process.GetProcessesByName(AppDomain.CurrentDomain.FriendlyName.Remove(AppDomain.CurrentDomain.FriendlyName.Length - 4));
+            if (pname.Length > 1)
+            {
+                var myProcessId = Process.GetCurrentProcess().Id;
+                pname.First(p => p.Id != myProcessId).Kill();
+            }
+
+        }
+
+        public static string GetAppName()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Name;
+        }
+    }
+}
